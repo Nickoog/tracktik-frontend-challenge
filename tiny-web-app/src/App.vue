81263132b9bc313cee98fdaaf2d3e154ded63362
Nicolas Hoog <nicolas.hoog@gmail.com>
2019-04-20 16:06:05 -0400

    setup sass file

--- tiny-web-app/src/App.vue
+++ tiny-web-app/src/App.vue
@@ -1,6 +1,12 @@
 <template>
     <div id="app">
         <h3>{{title}}</h3>
+        <div class="alert alert-success alert-dismissible fade show" role="alert">
+            With Bootstrap!
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+            </button>
+        </div>
     </div>
 </template> 
 
@@ -14,3 +20,7 @@
         }
     }
 </script>
+
+<style lang="scss">
+    @import './sass/vue';
+</style>
--- tiny-web-app/src/main.js
+++ tiny-web-app/src/main.js
@@ -1,6 +1,10 @@
 import Vue from 'vue';
 import App from './App.vue';
 
+// Bootstrap
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
 new Vue({
     el: '#app',
     render: h => h(App)
