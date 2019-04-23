5faf08aa27518a4553e6b35cd00cf570f6684c2a
Nicolas Hoog <nicolas.hoog@gmail.com>
2019-04-23 00:33:41 -0400

    added edit profil option

--- tiny-web-app/src/components/Modal.vue
+++ tiny-web-app/src/components/Modal.vue
@@ -15,26 +15,68 @@
                         <i class="fa fa-times-circle" aria-hidden="true"></i>
                     </button>
                 </header>
-                <section class="modal-body">
+                <section 
+                    class="modal-body"
+                    v-if="!success"
+                >
                     <p>
-                        <strong>Name:</strong> {{ value.givenName }}
+                        <strong>Name:</strong>
+                        <input 
+                            type="text" 
+                            v-model="value.givenName" 
+                            :readonly="readonly"   
+                        >
                     </p>
                     <p>
-                        <strong>Username: </strong>{{ value.username }}
+                        <strong>Username:</strong>
+                        <input 
+                            type="text" 
+                            v-model="value.username" 
+                            :readonly="readonly" 
+                        >
                     </p>
                     <p>
-                        <strong>Email:</strong> {{ value.email }}
+                        <strong>Email:</strong>
+                        <input 
+                            type="text" 
+                            v-model="value.email" 
+                            :readonly="readonly"
+                        >
                     </p>
+                    <footer class="modal-footer">
+                        <button
+                            v-if="readonly"
+                            type="button"
+                            class="btn btn-primary"
+                            @click="toggleReadonly"
+                        >
+                        Edit profile
+                        </button>
+                        <button
+                            v-if="!readonly"
+                            type="button"
+                            class="btn btn-primary"
+                            @click="edit"
+                        >
+                        Save change
+                        </button>
+                    </footer>
                 </section>
-                <footer class="modal-footer">
-                    <button
-                        type="button"
-                        class="btn btn-primary"
-                        @click="close"
-                    >
-                    Close
-                    </button>
-                </footer>
+                <section
+                    class="modal-body"
+                    v-if="success"
+                >
+                    <p class="text-center">Your changes have been saved</p>
+                    <footer class="modal-footer">
+                        <button
+                            type="button"
+                            class="btn btn-primary"
+                            @click="close"
+                        >
+                            Close
+                        </button>
+                    </footer>
+                </section>
             </div>
         </div>
     </transition>
@@ -43,11 +85,27 @@
 <script>
     export default {
         name: 'modal',
-        props: {value: Object},
+        props: {
+            value: Object,
+            success: Boolean
+        },
+        data() {
+            return {
+                readonly: true 
+            }
+        },
         methods: {
-            close() {
+            close () {
+                this.readonly = true;
+                this.$emit('success');
                 this.$emit('close');
             },
+            edit () {
+                this.$emit('edit');
+            },
+            toggleReadonly () {
+                this.readonly = false;
+            }
         },
     };
 </script>
